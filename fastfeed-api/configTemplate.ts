a2034f120998de0bea591aaef1d4b068a2af9b88
David McCoy <damccoy1@crimson.ua.edu>
2019-02-12 16:07:06 -0600

    updated with Twitter request example

---
+++ fastfeed-api/configTemplate.ts
@@ -0,0 +1,7 @@
+//Twitter API
+export const twitterKeys = {
+    consumer_key: '',
+     consumer_secret: '',
+     access_token_key: '',
+     access_token_secret: ''
+}
--- fastfeed-api/functions/src/index.ts
+++ fastfeed-api/functions/src/index.ts
@@ -1,8 +1,37 @@
 import * as functions from 'firebase-functions';
+import * as Twitter from 'twitter';
+import * as config from './config';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
+
+var client = new Twitter({
+    consumer_key: config.twitterKeys["consumer_key"],
+    consumer_secret: config.twitterKeys["consumer_secret"],
+    access_token_key: config.twitterKeys["access_token_key"],
+    access_token_secret: config.twitterKeys["access_token_secret"]
+  });
+
+export const getTwitterFollowing = functions.https.onRequest((request, response) => {
+    client.get('friends/list', { screen_name: "rtrPAWLfan" }, function(error, tweets, res) {
+        if (error) throw error;
+        response.send({
+            "tweets": tweets,
+            "users": JSON.parse(res["body"])["users"]
+        });
+    });
+});
+
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    if ("name" in request.body) {
+        response.send({ 
+            "message": `Sup ${request.body["name"]}!`
+        });
+    }
+    else {
+        response.send({
+            "error": "No name was provided"
+        });
+    }
 });
