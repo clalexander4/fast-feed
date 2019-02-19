36f34c83e4a081b71873394cee8e49269d6d1d58
Jacoby Benger <jacobybenger@gmail.com>
2019-02-19 12:32:18 -0600

    Testing the git ignore, seem to keep having problems.

---
+++ fastfeed-api/functions/src/configTemplate.ts
@@ -0,0 +1,8 @@
+//Twitter API
+//Jacoby's Keys
+export const twitterKeys = {
+    consumer_key: 'J894kcO1EvOxOAwbmDF7nUrJP',
+     consumer_secret: 'eZYtTbEm0cLPmPrAh65eNVrVBkNrn0iDUbYyPCLSsroepxo3Bu',
+     access_token_key: '1024278774140035072-zVE1ato6KI9of7hBRfTzrBspAdixJg',
+     access_token_secret: '0ozPErCa6Zf5b3M7U3TpXRer2bbYDYBNrjITOfwqwBF8E'
+}
