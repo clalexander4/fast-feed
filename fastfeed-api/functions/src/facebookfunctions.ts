162e2df1b3a2011fdaec479f61ebeb16c3639c9f
ctgannaway <ctgannaway@crimson.ua.edu>
2019-02-19 16:57:26 -0600

    Facebook Functions begining

---
+++ fastfeed-api/functions/src/facebookfunctions.ts
@@ -0,0 +1,18 @@
+import * as functions from 'firebase-functions';
+import FB from 'fb';
+
+// ES5
+var FB = require('fb'),
+    fb = new FB.Facebook(options);
+
+    FB.setAccessToken('access_token');
+    var accessToken = FB.getAccessToken();
+
+    FB.api('4', { fields: ['id', 'name'] }, function (res) {
+        if(!res || res.error) {
+          console.log(!res ? 'error occurred' : res.error);
+          return;
+        }
+        console.log(res.id);
+        console.log(res.name);
+      });
