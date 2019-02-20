f6a6a13497378afcb9c038445ed8e67b86ca0db3
David McCoy <damccoy1@crimson.ua.edu>
2019-02-19 22:28:31 -0600

    made waffle ripper pull from the interwebs

--- waffle-rip/waffleRip.py
+++ waffle-rip/waffleRip.py
@@ -1,13 +1,14 @@
-from json import loads
+import json
 import csv
+import urllib.request
 
 with open('input.json', 'r') as file:
-    data = file.read().replace('\n', '')
-    with open('output.csv', 'wb') as csvfile:
+    data = json.loads(urllib.request.urlopen("https://api.waffle.io/damccoy1/fast-feed/cards").read().decode('utf8').replace("'", '"'))
+    with open('ProjectBacklog.csv', 'w') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
         filewriter.writerow(['ID','Story','Estimation (hours)','Priority','Sprint When Finished'])
-        for item in loads(data):
+        for item in data:
             print(item)
             milestone = "n/a"
             if "milestone" in item["githubMetadata"]:  
