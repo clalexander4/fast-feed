8538f63cc10488f930dfc4ac010a1bdc0e422c73
David McCoy <damccoy1@crimson.ua.edu>
2019-02-19 21:42:42 -0600

    fixed android tomfoolery

--- fastfeed-mobile/fastfeed/lib/Classes/auth.dart
+++ fastfeed-mobile/fastfeed/lib/Classes/auth.dart
@@ -3,11 +3,11 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rxdart/rxdart.dart';
 
-class AuthService {
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final Firestore _db = Firestore.instance;
+final GoogleSignIn _googleSignIn = GoogleSignIn();
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final Firestore _db = Firestore.instance;
 
+class AuthService {
   Observable<FirebaseUser> user; // firebase user
   Observable<Map<String, dynamic>> profile; // custom user data in Firestore
   PublishSubject loading = PublishSubject();
