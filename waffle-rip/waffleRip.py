3f0667bb33eda3b1f259b7855caae64a4f9239c2
David McCoy <damccoy1@crimson.ua.edu>
2019-02-20 19:49:23 -0600

    make output for sprint backlog

--- waffle-rip/waffleRip.py
+++ waffle-rip/waffleRip.py
@@ -4,6 +4,10 @@ import urllib.request
 
 with open('input.json', 'r') as file:
     data = json.loads(urllib.request.urlopen("https://api.waffle.io/damccoy1/fast-feed/cards").read().decode('utf8').replace("'", '"'))
+    for item in data:
+        item["description"] = json.loads(urllib.request.urlopen("https://api.github.com/repos/damccoy1/fast-feed/issues/" + str(item["githubMetadata"]["number"])).read().decode('utf8').replace("'", '"'))["body"]
+    with open("sample.json", "w") as out:
+        out.write(json.dumps(data))
     with open('ProjectBacklog.csv', 'w') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
@@ -14,3 +18,8 @@ with open('input.json', 'r') as file:
             if "milestone" in item["githubMetadata"]:  
                 milestone = item["githubMetadata"]["milestone"]["title"]
             filewriter.writerow([item["githubMetadata"]["title"],milestone,item["githubMetadata"]["number"],"1","1"])
+    with open('SprintGoalBacklog.txt', 'w') as txtfile:
+        for item in data:
+            if not item["description"]:
+                item["description"] = ""
+            txtfile.write(item["githubMetadata"]["title"]+"\n"+item["description"]+"\n")
