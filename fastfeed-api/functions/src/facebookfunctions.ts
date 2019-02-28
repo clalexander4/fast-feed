94e8b71d74e43ffcbe855d212a15d433f6c28dd3
ctgannaway <ctgannaway@crimson.ua.edu>
2019-02-28 14:01:41 -0600

    Facebook Updates

--- fastfeed-api/functions/src/facebookfunctions.ts
+++ fastfeed-api/functions/src/facebookfunctions.ts
@@ -1,18 +1,13 @@
 import * as functions from 'firebase-functions';
-import FB from 'fb';
+// import * from 'fb';
 
-// ES5
-var FB = require('fb'),
-    fb = new FB.Facebook(options);
-
-    FB.setAccessToken('access_token');
-    var accessToken = FB.getAccessToken();
-
-    FB.api('4', { fields: ['id', 'name'] }, function (res) {
-        if(!res || res.error) {
-          console.log(!res ? 'error occurred' : res.error);
-          return;
-        }
-        console.log(res.id);
-        console.log(res.name);
-      });
+export const getFBdata = functions.https.onRequest((request, response) => {
+  FB.api('4', function (res) {
+    if(!res || res.error) {
+     console.log(!res ? 'error occurred' : res.error);
+     return;
+    }
+    console.log(res.id);
+    console.log(res.name);
+  });
+});
