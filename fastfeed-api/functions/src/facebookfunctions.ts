0c6cb9b68265cae0eeea96353d03b52b3097aa33
ctgannaway <ctgannaway@crimson.ua.edu>
2019-02-28 15:51:05 -0600

    Facebook Get Functions working

--- fastfeed-api/functions/src/facebookfunctions.ts
+++ fastfeed-api/functions/src/facebookfunctions.ts
@@ -1,10 +1,7 @@
 import * as functions from 'firebase-functions';
 
-export const getFbLikes = function (response: functions.Response) {
-  var FB = require('fb');
-  FB.options({version: 'v2.4'});
-  FB.setAccessToken('EAATeoZCPkzmUBAJ8X9PlC88N9qqSZAEMChY9TsgaOyflqPYo1DMPzmKGXrSQevE6s6zbFkBmSSWvjiPqyuIbK6ZBZCsJUMJKyMOp1M5JCZBSW9SXDaBT1AiHZBsJbwEQj0Okq8QB3ZAyXjYoZBBIZAEndONLu7ZCh7IdfQlUC7ko47J3Rx3iJ7ZAcGLkMuZA8kyL2BcZD');
-  FB.api('10213329836548424', { fields: ['id','name', 'likes'] }, function (res:any) {
+export const getFbLikes = function (FB: any,response: functions.Response) {
+  FB.api(FB.id, { fields: ['id','name', 'likes'] }, function (res:any) {
       if(!res || res.error) {
       console.log(!res ? 'error occurred' : res.error);
       response.send({
@@ -22,11 +19,8 @@ export const getFbLikes = function (response: functions.Response) {
       });
   });
 };
-export const getFbUserFeed = function (response: functions.Response) {
-  var FB = require('fb');
-  FB.options({version: 'v2.4'});
-  FB.setAccessToken('EAATeoZCPkzmUBAJ8X9PlC88N9qqSZAEMChY9TsgaOyflqPYo1DMPzmKGXrSQevE6s6zbFkBmSSWvjiPqyuIbK6ZBZCsJUMJKyMOp1M5JCZBSW9SXDaBT1AiHZBsJbwEQj0Okq8QB3ZAyXjYoZBBIZAEndONLu7ZCh7IdfQlUC7ko47J3Rx3iJ7ZAcGLkMuZA8kyL2BcZD');
-  FB.api('987160298144095', { fields: ['posts','picture'] }, function (res:any) {
+export const getFbUserFeed = function (FB: any,response: functions.Response) {
+  FB.api(FB.id, { fields: ['posts','picture'] }, function (res:any) {
       if(!res || res.error) {
       console.log(!res ? 'error occurred' : res.error);
       response.send({
